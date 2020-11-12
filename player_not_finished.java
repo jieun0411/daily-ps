718e332f308dcf7093575cdb46002b075cc2f768
jieun0411 <annakim0411@naver.com>
2020-11-12 17:49:54 +0900

    첫 번째 커밋

---
+++ player_not_finished.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class player_not_finished {
+    public static void main(String args[]){
+        String[] participant={"a","b","c","d"};
+        String[] completion={"a","d","c"};
+
+        System.out.println(solution(participant,completion));
+    }
+
+    public static String solution(String[] participant, String[] completion){
+
+        List<String> listP = new ArrayList<>(Arrays.asList(participant));
+        List<String> listC = new ArrayList<>(Arrays.asList(completion));
+
+        for(int i=0; i< completion.length;i++) {
+            listP.remove(listC.get(i));
+        }
+        String answer= listP.get(0);
+        return answer;
+    }
+}
