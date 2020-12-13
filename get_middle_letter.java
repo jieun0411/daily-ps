8f2166d30a71410579f1f7bc2150d230e748fa49
jieun0411 <annakim0411@naver.com>
2020-12-13 22:56:21 +0900

    프로그래머스 가운데 글자 가져오기

--- get_middle_letter.java
+++ get_middle_letter.java
@@ -7,13 +7,10 @@ public class get_middle_letter {
         public static String solution(String s) {
             String answer = "";
             int len=s.length();
-            if(len%2==0){
+            if(len%2==0)
                answer= s.substring(len/2-1,len/2+1);
-            }
-            else{
+            else
                 answer=s.substring(len/2,len/2+1);
-            }
-
             return answer;
         }
 }
