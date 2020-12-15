aecdf5c9958b79933f545033c036ede07a115d2e
jieun0411 <annakim0411@naver.com>
2020-12-15 23:39:46 +0900

    프로그래머스

---
+++ the_sum_of_divisor.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class the_sum_of_divisor {
+    public static void main(String[] args){
+        System.out.println(solution(5));
+    }
+    public static int solution(int n) {
+        int answer = 1;
+        for(int i=2;i<=n;i++)
+            if(n%i==0)
+                answer+=i;
+        return answer;
+    }
+}
