f7ea8ffc0a909614d417bed316a7f6c1af7c85c7
jieun0411 <annakim0411@naver.com>
2020-12-16 09:31:05 +0900

    프로그래머스 자릿수 더하기

---
+++ add_digits.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class add_digits {
+    public static void main(String[] args){
+        System.out.println(solution(123));
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        while(n>0){
+            answer+=n%10;
+            n/=10;
+        }
+        return answer;
+    }
+}
