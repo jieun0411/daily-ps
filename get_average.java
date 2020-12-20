b86d8cab5d7a1182a758e8955949999c02592284
jieun0411 <annakim0411@naver.com>
2020-12-20 22:48:48 +0900

    프로그래머스 평균구하기

---
+++ get_average.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class get_average {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4};
+        System.out.println(solution(arr));
+    }
+    public static double solution(int[] arr) {
+        double answer = 0;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        answer=(double)sum/arr.length;
+        return answer;
+    }
+}
