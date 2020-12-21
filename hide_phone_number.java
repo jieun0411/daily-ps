0c996ce9466953d5c274926e4d93234f57fdf22b
jieun0411 <annakim0411@naver.com>
2020-12-21 17:50:24 +0900

    프로그래머스 핸드폰 번호 가리기

---
+++ hide_phone_number.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class hide_phone_number {
+    public static void main(String[] args){
+        System.out.println(solution("01033333333"));
+    }
+    public static String solution(String phone_number) {
+        String answer = "";
+        for(int i = 0; i < phone_number.length(); i++) {
+            if(i < phone_number.length()-4){
+                answer += "*";
+            }
+            else{ answer += phone_number.charAt(i); } }
+        return answer;
+    }
+}
